c79631dccc3f2f9191fdafb37c664af10a314430
unknown <hafidardiansyahh@gmail.com>
2020-12-08 09:33:10 +0700

    add bootstrap

---
+++ resources/views/add.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('title', 'Add')
+
+@section('head')
+    <style>
+        body {
+            background-color: red;
+        }
+
+    </style>
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>Add</h1>
+    </div>
+@endsection
