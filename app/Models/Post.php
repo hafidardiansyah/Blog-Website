34c2ec5ae7301837f49e2be3b8c3538c108bcd0e
unknown <hafidardiansyahh@gmail.com>
2020-12-09 09:21:59 +0700

    protected create form post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,4 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = ['title', 'slug', 'body'];
+    // protected $guarded = []; for admin because not recomended for user
 }
